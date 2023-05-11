9d5685545e65413eae965f06b7d570bcb82e370a
Michał Kulma <michal.kulma@ZS1MM.LOCAL>
2023-05-11 15:56:09 +0200

    .

---
+++ app.js
@@ -0,0 +1,54 @@
+var map = L.map('map').setView([52.44251260319042, 18.921683024307846], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function liczba(){
+    return Math.floor(Math.random() * wojewodztwa.features.length-1)
+}
+
+console.log(wojewodztwa.features)
+
+var points=0
+
+
+
+
+function start(){
+     licz=liczba()
+    const dol=document.getElementById('woj')
+    
+    var wojewodztwo=wojewodztwa.features[licz].properties.nazwa
+    
+    dol.innerHTML=wojewodztwo
+}
+
+var warstwa=[]
+for(let i=0;i<=wojewodztwa.features.length-1;i++){
+    
+    var wojewodztwo=wojewodztwa.features[i]
+    var mapwoj= L.geoJSON(wojewodztwo).addTo(map)
+    
+    mapwoj.on('click',function(e){
+        
+const wybrane=e.layer.feature.properties.nazwa
+console.log(wybrane)
+
+        if(wybrane==wojewodztwa.features[licz].properties.nazwa){
+            document.getElementById('odp').innerHTML='dobrze'
+            points=points+1
+            document.getElementById('punkty').innerHTML=points
+
+        }
+else{ 
+    document.getElementById('odp').innerHTML='źle'
+
+
+
+start()
+}   
+
+})      
+}
